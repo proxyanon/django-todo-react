8f20cfacc5acae5fe2eed9bfbe2202fb843e6062
Daniel Freire <ondproxy@gmail.com>
2022-10-25 13:47:19 -0300

    Update safeSQL.ts

--- utils/safeSQL.ts
+++ utils/safeSQL.ts
@@ -1,3 +1,3 @@
 export function safeSQL(sqlQuery: string): string {
-  return sqlQuery.replace(/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/, "");
+  return sqlQuery.replace(/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/, ""); // letters (lower, upper), numbers, spaces
 }
