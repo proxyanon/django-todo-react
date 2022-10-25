ad83f12d3dd3660413bf1980165e571ba5248db3
Daniel Freire <ondproxy@gmail.com>
2022-10-25 12:44:39 -0300

    Update isEmail.ts

--- utils/isEmail.ts
+++ utils/isEmail.ts
@@ -1,5 +1,5 @@
 export function isEmail(email: string, allowedServices: Array<string>): boolean {
-  if (!email.includes('@') || !email.includes('.') || !allowedServices.includes(email.split('.')[1])) {
+  if (!email.includes('@') || !email.includes('.') || !allowedServices.includes(email.split('@')[1].split('.')[0])) {
     return false;
   }
 
